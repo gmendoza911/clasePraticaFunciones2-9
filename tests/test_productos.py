d8cb0e1b71dbd24c08824fd6d2978c5d51ebcc49
brayann.farfan@bue.edu.ar <brayann.farfan@bue.edu.ar>
2026-09-16 19:29:22 -0300

    clase practica de parametrize con mark personalizados

--- tests/test_productos.py
+++ tests/test_productos.py
@@ -2,24 +2,6 @@ import pytest
 import productos
 
 
-# decoradores
-@pytest.fixture(autouse=True)
-def limpiar_productos():
-    productos.productos.clear()
-    yield # => se ejecuta los test de prueba
-    productos.productos.clear()
-
-@pytest.fixture
-def datos_base():
-    productos.productos.extend([
-        {"nombre":"Mouse","precio":1000,"cantidad":3},
-        {"nombre":"Teclado","precio":5000,"cantidad":2},
-        {"nombre":"Parlante","precio":3000,"cantidad":5}
-    ])
-
-    return productos.productos
-
-
 @pytest.mark.busqueda
 @pytest.mark.parametrize("datos_entradas,resultado_esperado",[
     (["Mouse","1500","3"] ,
@@ -69,7 +51,7 @@ def test_agregar_producto_exitos( monkeypatch ):
 
     assert len(productos.productos) > 0
 
-
+@pytest.mark.eliminar_producto
 def test_buscar_productos_existentes( datos_base ):
 
     resultado = productos.buscar_por_precio( datos_base, precio_maximo=4000)
@@ -84,7 +66,7 @@ def test_agregar_producto_precio_negativo( monkeypatch):
 
     assert len(productos.productos) == 0
 
-@pytest.mark.solis
+@pytest.mark.eliminar_producto
 def test_eliminar_producto( monkeypatch, datos_base ):
 
     monkeypatch.setattr("builtins.input",lambda _: "Mouse")
